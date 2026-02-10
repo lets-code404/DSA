cfe4b23f02d7f51a314a2d5de465284f8c6ff35f
lets-code404 <yashanshrastogi@gmail.com>
2026-02-10 15:05:08 +0530

    `Added LeetCode 204 solution: Count Primes`

--- 204._Count_Primes.cpp
+++ 204._Count_Primes.cpp
@@ -1,2 +1,38 @@
 // Leetcode 204. Count Primes
 
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int countPrimes(int n) {
+    // Sieve of Eratosthenes
+    if (n <= 2) return 0;
+
+    int count = 0;
+    vector<bool> prime(n, true);
+    prime[0] = prime[1] = false;
+
+    for (int i = 2; i * i < n; i++) {
+        if (prime[i]) {
+            for (int j = i * i; j < n; j += i) {
+                prime[j] = false;
+            }
+        }
+    }
+
+    for (int i = 2; i < n; i++) {
+        if (prime[i]) count++;
+    }
+    return count;
+}
+
+int main() {
+
+    int n = 30;
+
+    cout << "Number of primes less than " << n << " = "
+         << countPrimes(n) << endl;
+
+    return 0;
+}
